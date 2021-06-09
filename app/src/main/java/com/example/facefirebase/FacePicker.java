aadc07286c63fe01acdc42981ba5d9d883154190
Tuan07101999 <Tuan07107101999@gmail.com>
2021-06-09 10:33:02 +0700

    first commit

---
+++ app/src/main/java/com/example/facefirebase/FacePicker.java
@@ -0,0 +1,14 @@
+package com.example.facefirebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FacePicker extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_face_picker);
+    }
+}
